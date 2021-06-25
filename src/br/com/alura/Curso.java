e80979ea192c977b10a5224738a174fe33bd267b
Leonardo Mafra <domafras@gmail.com>
2021-06-25 10:46:01 -0300

    Quarta aula

--- src/br/com/alura/Curso.java
+++ src/br/com/alura/Curso.java
@@ -26,9 +26,27 @@ public class Curso {
 	public List<Aula> getAulas() {
 		return Collections.unmodifiableList(aulas);
 	}
-	
+
 	public void adiciona(Aula aula) {
 		this.aulas.add(aula);
 	}
 
+//	public int getTempoTotal() {
+//		int tempoTotal = 0;
+//		for (Aula aula : aulas) {
+//			tempoTotal += aula.getTempo();
+//		}
+//		return tempoTotal;
+//	}
+
+	// Usando stream()
+	public int getTempoTotal() {
+		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+	}
+
+	@Override
+	public String toString() {
+		return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal() + "," + " aulas: " + this.aulas + "]";
+	}
+	
 }
