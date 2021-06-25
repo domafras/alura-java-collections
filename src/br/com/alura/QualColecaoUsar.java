a6f1eb00109b9fc9a737de201be9294abe2dc207
Leonardo Mafra <domafras@gmail.com>
2021-06-25 12:25:29 -0300

    Nona aula

---
+++ src/br/com/alura/QualColecaoUsar.java
@@ -0,0 +1,17 @@
+package br.com.alura;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class QualColecaoUsar {
+
+	public static void main(String[] args) {
+		
+		//Sendo genérico com 'Collection'
+		Collection<Aluno> alunos = new HashSet<>();
+		
+		alunos.size();
+		
+	}
+	
+}
